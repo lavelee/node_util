8170bb5aec0a3ff13aeb9d8bf62f227e1a9da675
lavelee <lavelee15@gmail.com>
2019-04-21 07:21:38 +0000

    get/set for deep obj

---
+++ src/deep-obj/deep.js
@@ -0,0 +1,40 @@
+function parseSelector(selector) {
+    if (!selector_str) throw "selector required";
+    if (typeof selector !== 'string') throw "Selector should be string";
+    return selector.split(".").filter(d=>d);
+  }
+  
+  function stepInto(obj, arg, force_dig){
+    let v = obj[arg];
+    if (typeof v === 'object' && v !== null) return v;
+    else if (v) throw `key '${arg}' has a value '${v}' - which is not an object`;
+    else if (v === undefined) return (force_dig) ? obj[arg] = {} : {};
+  }
+  
+  function lastObj(obj, args, force_dig=true){
+    if (args.length > 0){
+      let inner_obj = stepInto(obj, args.shift());
+      return lastObj(inner_obj, args, force_dig);
+    } else {
+      return obj;
+    }
+  }
+  
+  function deepSet(obj, selector, value){
+    if (!value) throw "value required";
+    let args = parseSelector(selector);
+    let deepest_arg = args.pop();
+    lastObj(obj, args)[deepest_arg] = value;
+    return obj;
+  }
+  
+  function deepGet(obj, selector){
+    let args = parseSelector(selector);
+    let deepest_arg = args.pop();
+    return lastObj(obj, args, true)[deepest_arg]; 
+  }
+
+module.exports = {
+  deepSet,
+  deepGet,
+}
